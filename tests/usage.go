8459efef125e9989e396f14a971d619c67e7fd9a
Florian Kunzweiler <kunzweiler@raceresult.com>
2021-11-23 17:10:24 +0100

    updated usage.go for xmllint

--- tests/usage.go
+++ tests/usage.go
@@ -9,14 +9,14 @@ import (
 
 func main() {
 	doc := &sepa.Document{}
-	if err := doc.InitDoc("MSGID", "2017-06-07T14:39:33", "2017-06-09",
-		"Emiter Name", "FR1420041010050500013M02606", "BKAUATWW", "DEBZ76J8G5",
+	if err := doc.InitDoc("MSGID", "2017-06-07T14:39:33", "2017-06-07T14:39:33",
+		"2017-06-11", "Emiter Name", "FR1420041010050500013M02606", "BKAUATWW",
 		"US", "Your Street 120", "76657 Your City, Country"); err != nil {
 		log.Fatal("can't create sepa document : ", err)
 	}
 
 	if err := doc.AddTransaction("F201705", 70000, "EUR", "DEV Electronics",
-		"GB29NWBK60161331926819", "Invoice 12345", "BFAUAUWA"); err != nil {
+		"GB29NWBK60161331926819", "BFAUAUWA", "Invoice 12345"); err != nil {
 		log.Fatal("can't add transaction in the sepa document : ", err)
 	}
 
