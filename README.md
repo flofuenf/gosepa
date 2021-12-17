e227ebfba270132ff52ae6377b84d307be22aa30
Florian Kunzweiler <kunzweiler@raceresult.com>
2021-12-17 13:03:16 +0100

    updated usage

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ $ go get github.com/flofuenf/gosepa/sepa
 ```
 
 ## Usage
+### Direct Debit
 
 ```go
 package main
@@ -27,6 +28,7 @@ import (
 )
 
 func main() {
+	// Direct Debit
 	doc := &sepa.DirectDebit{}
 	if err := doc.InitDoc("MSGID", "2017-06-07T14:39:33", "2017-06-07T14:39:33",
 		"2017-06-11", "Emiter Name", "FR1420041010050500013M02606", "BKAUATWW",
@@ -40,6 +42,41 @@ func main() {
 	}
 
 	res, err := doc.PrettySerialize()
+	if err != nil {
+		log.Fatal("can't get the xml doc : ", err)
+	}
+
+	fmt.Println(string(res))
+}
+```
+
+### Credit Transfer
+
+```go
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/flofuenf/gosepa/sepa"
+)
+
+func main() {
+	// Credit Transfer
+	ctXML := &sepa.CreditTransfer{}
+	if err := ctXML.InitDoc("MSGID", "paymentInfoID", "2017-06-07T14:39:33",
+		"2017-06-11", "Emitter Name", "FR1420041010050500013M02606", "BKAUATWW",
+		"US", "Your Street 120", "76657 Your City, Country"); err != nil {
+		log.Fatal("can't create sepa credit transfer document : ", err)
+	}
+
+	if err := ddXML.AddTransaction("F201705", 70000, "EUR", "DEV Electronics",
+		"GB29NWBK60161331926819", "BFAUAUWA", "Invoice 12345", "mandandtIT", "2017-06-07T14:39:33"); err != nil {
+		log.Fatal("can't add transaction in the sepa document : ", err)
+	}
+
+	res, err = ddXML.PrettySerialize()
 	if err != nil {
 		log.Fatal("can't get the xml doc : ", err)
 	}
--- tests/usage.go
+++ tests/usage.go
@@ -8,19 +8,40 @@ import (
 )
 
 func main() {
-	doc := &sepa.DirectDebit{}
-	if err := doc.InitDoc("MSGID", "2017-06-07T14:39:33", "2017-06-07T14:39:33",
-		"2017-06-11", "Emiter Name", "FR1420041010050500013M02606", "BKAUATWW",
+	// Direct Debit
+	ddXML := &sepa.DirectDebit{}
+	if err := ddXML.InitDoc("MSGID", "2017-06-07T14:39:33", "2017-06-07T14:39:33",
+		"2017-06-11", "Emitter Name", "FR1420041010050500013M02606", "BKAUATWW",
 		"emitterID", "US", "Your Street 120", "76657 Your City, Country"); err != nil {
-		log.Fatal("can't create sepa document : ", err)
+		log.Fatal("can't create sepa direct debit document : ", err)
 	}
 
-	if err := doc.AddTransaction("F201705", 70000, "EUR", "DEV Electronics",
+	if err := ddXML.AddTransaction("F201705", 70000, "EUR", "DEV Electronics",
 		"GB29NWBK60161331926819", "BFAUAUWA", "Invoice 12345", "mandandtIT", "2017-06-07T14:39:33"); err != nil {
 		log.Fatal("can't add transaction in the sepa document : ", err)
 	}
 
-	res, err := doc.PrettySerialize()
+	res, err := ddXML.PrettySerialize()
+	if err != nil {
+		log.Fatal("can't get the xml doc : ", err)
+	}
+
+	fmt.Println(string(res))
+
+	// Credit Transfer
+	ctXML := &sepa.CreditTransfer{}
+	if err := ctXML.InitDoc("MSGID", "paymentInfoID", "2017-06-07T14:39:33",
+		"2017-06-11", "Emitter Name", "FR1420041010050500013M02606", "BKAUATWW",
+		"US", "Your Street 120", "76657 Your City, Country"); err != nil {
+		log.Fatal("can't create sepa credit transfer document : ", err)
+	}
+
+	if err := ddXML.AddTransaction("F201705", 70000, "EUR", "DEV Electronics",
+		"GB29NWBK60161331926819", "BFAUAUWA", "Invoice 12345", "mandandtIT", "2017-06-07T14:39:33"); err != nil {
+		log.Fatal("can't add transaction in the sepa document : ", err)
+	}
+
+	res, err = ddXML.PrettySerialize()
 	if err != nil {
 		log.Fatal("can't get the xml doc : ", err)
 	}
